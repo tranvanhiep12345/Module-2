59e924830897c851acfcdd297a9eb69939e8e3dc
Hiep <tranvanhiep992001@gmail.com>
2023-05-15 15:34:57 +0700

    bai tap

---
+++ luyentapdestructuring.js
@@ -0,0 +1,9 @@
+let profile = {
+    fname: 'Code',
+    lname: 'Gym',
+    bday: new Date('1979-01-02')
+}
+let {fname,lname}= profile
+console.log({fname,lname})
+console.log(fname)
+console.log(lname)
