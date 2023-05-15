39a7adc74f2ee00b705deed81defa3a20f28777b
Hiep <tranvanhiep992001@gmail.com>
2023-05-15 15:04:22 +0700

    bai tap

---
+++ spreadSyntax.js
@@ -0,0 +1,12 @@
+let object1 = {
+    Name: "Hiep",
+    Age: 22,
+    Year: 2001
+}
+let object2 = {
+    Name: "david",
+    Age: 24,
+    Year: 1995
+}
+let object = {...object2,...object1}
+console.log(object)
